a88f5300d909e0a93bffd31e8b0a457dca194ed7
ModestYar <ModestYar@gmail.com>
2023-05-01 18:08:20 +0300

    first commit

---
+++ Controller.cs
@@ -0,0 +1,228 @@
+﻿using Mohostani;
+using System;
+using VkNet.Enums.SafetyEnums;
+using VkNet.Model.RequestParams;
+using VkNet.Model;
+
+namespace moxbot
+{
+    internal class Controller
+    {
+        
+        public static void MessageToChat(string UserMessage, long? peerId) // отправка текстовых сообщений по команде
+        {
+
+            if (UserMessage == "я")
+
+            {
+                var message = new MessagesSendParams
+
+                {
+                    Message = "крутой",
+
+                    PeerId = peerId,
+                    RandomId = 0,
+
+                    Intent = Intent.Default
+
+
+                };
+
+                Program.api.Messages.SendAsync(message);
+
+            }
+        }
+
+        public static void StickerToChat(string UserMessage, long? peerId) // отправка текстовых сообщений по команде
+        {
+            Program program = new Program();
+
+            if (UserMessage == "олег")
+
+            {
+
+                var message = new MessagesSendParams
+
+                {
+                    StickerId = 76104,
+
+                    PeerId = peerId,
+                    RandomId = 0,
+
+                    Intent = Intent.Default
+
+                };
+
+                Program.api.Messages.SendAsync(message);
+
+            }
+        }
+
+        public static void Roullete(string UserMessage, long? peerId) // выбор случайного пользователя по команде
+        {
+            if (UserMessage == "кто мох")
+            {
+                var randomuser = Program.api.Messages.GetConversationMembers((long)peerId);
+
+                Random rnd = new Random();
+
+
+                var message = new MessagesSendParams
+
+                {
+                    Message = randomuser.Profiles[rnd.Next(0, randomuser.Profiles.Count)].FirstName,
+
+                    PeerId = peerId,
+                    RandomId = 0,
+
+                    Intent = Intent.Default
+
+
+                };
+
+                Program.api.Messages.SendAsync(message);
+
+            }
+        }
+
+        public static void TextAniimation(string UserMessage, long? peerId, long? ChatMessageId)
+        {
+            if (UserMessage == "грузи")
+
+            {
+
+                var message = new MessagesSendParams
+
+                {
+                    Message = "ОК",
+
+                    PeerId = peerId,
+                    RandomId = 0,
+
+                    Intent = Intent.Default
+
+
+                };
+
+                var g = Program.api.Messages.Send(message);
+
+                for (int i = 0; i < 4; i++)
+                {
+
+                    Program.api.Messages.Edit(new MessageEditParams
+                    {
+                        Message = "Загрузка",
+
+                        ConversationMessageId = ChatMessageId + 1,
+                        PeerId = (long)peerId,
+                    });
+
+                    Thread.Sleep(500);
+
+                    Program.api.Messages.Edit(new MessageEditParams
+                    {
+                        Message = "Загрузка.",
+
+                        ConversationMessageId = ChatMessageId + 1,
+                        PeerId = (long)peerId,
+
+                    });
+                    Thread.Sleep(500);
+                    Program.api.Messages.Edit(new MessageEditParams
+                    {
+                        Message = "Загрузка..",
+
+                        ConversationMessageId = ChatMessageId + 1,
+
+                        PeerId = (long)peerId,
+
+                    });
+                    Thread.Sleep(500);
+                    Program.api.Messages.Edit(new MessageEditParams
+                    {
+                        Message = "Загрузка...",
+
+                        ConversationMessageId = ChatMessageId + 1,
+
+                        PeerId = (long)peerId,
+
+                    });
+                    Thread.Sleep(500);
+                }
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "О",
+
+                    ConversationMessageId = ChatMessageId + 1,
+                    PeerId = (long)peerId,
+                });
+
+                Thread.Sleep(500);
+
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛ",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+                });
+
+                Thread.Sleep(500);
+
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛЕ",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+                });
+
+                Thread.Sleep(500);
+
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛЕГ",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+                });
+
+                Thread.Sleep(500);
+
+                    Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛЕГ М",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+                });
+
+                Thread.Sleep(500);
+
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛЕГ МО",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+
+                });
+                Thread.Sleep(500);
+                Program.api.Messages.Edit(new MessageEditParams
+                {
+                    Message = "ОЛЕГ МОХ",
+
+                    ConversationMessageId = ChatMessageId + 1,
+
+                    PeerId = (long)peerId,
+
+                });
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+using VkNet.Model.GroupUpdate;
+using VkNet.Enums.SafetyEnums;
+using Ini;
+using moxbot;
+
+namespace Mohostani
+{
+    class Program
+    {
+        static string[] lines = File.ReadLines("Config.ini").ToArray();
+        static Dictionary<string, Ini.Category> result = Ini.Parser.Parse(lines);
+        static Category BotConfig = result["BotConfig"];
+
+        static string key = BotConfig.GetEntryByKey("Key").Value;
+        static int groupId = Convert.ToInt32(BotConfig.GetEntryByKey("GroupID").Value);
+        public static VkApi api = new VkApi();
+        public string? UserMessage { get; set; }
+        public long? PeerId { get; set; }
+        public long? ChatMessageId { get; set; }
+
+        
+        static void Main(string[] args)
+        {
+            Auth();
+
+            LongPoll(); 
+        }
+        public static void Auth() // авторизация
+        {
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = key
+            });
+            Console.WriteLine($"ВОТ ТВОЙ ТОКЕН МУЖИК\t {api.Token} \t ПОХОДУ РОБИТ");
+        } 
+
+
+        public static void LongPoll() // методы отправки данных вынесены сюда
+        {
+      
+            Program getUserMes = new Program();
+            Program getPeerId = new Program();
+            Program getChatMesId = new Program();
+
+
+            while (true)
+            {
+                var s = api.Groups.GetLongPollServer(ulong.Parse(groupId.ToString()));
+
+                var poll = api.Groups.GetBotsLongPollHistory(new BotsLongPollHistoryParams()
+                {
+                    Server = s.Server,
+
+                    Ts = s.Ts,
+
+                    Key = s.Key,
+
+                    Wait = 25
+
+                });
+
+                //проверяем появилось ли новое событие
+
+                if (poll?.Updates == null)
+                {
+                    continue;
+                }
+                //»копаемся» в новом событие
+
+                foreach (var a in poll.Updates)
+                {
+                    if (a.Type.Value == GroupUpdateType.MessageNew)
+                    {
+
+                      getUserMes.UserMessage = ((MessageNew)a.Instance).Message.Text.ToLower();
+
+                        getPeerId.PeerId = ((MessageNew)a.Instance).Message.PeerId;
+
+                        getChatMesId.ChatMessageId = ((VkNet.Model.GroupUpdate.MessageNew)a.Instance).Message.ConversationMessageId;
+
+                        Controller.MessageToChat (getUserMes.UserMessage, getPeerId.PeerId);
+
+                        Controller.StickerToChat(getUserMes.UserMessage, getPeerId.PeerId);
+
+                        Controller.Roullete(getUserMes.UserMessage, getPeerId.PeerId);
+
+                        Controller.TextAniimation(getUserMes.UserMessage, getPeerId.PeerId, getChatMesId.ChatMessageId);
+                    }
+                }
+
+            }
+        }    
+
+ 
+    }
+}
